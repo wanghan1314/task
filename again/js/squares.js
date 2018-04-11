dcca84b8a5b5138e8dcf32b9cfc5fef493939edd
wanghan1314 <31241903+wanghan1314@users.noreply.github.com>
2018-04-11 17:12:29 +0800

    11

--- again/js/squares.js
+++ again/js/squares.js
@@ -1,11 +1,11 @@
 var timer;
 var div=document.getElementsByTagName("div");
-var colors=['red ','blue','yellow','orange','pink'];
+var colors=['red','blue','purple','yellow','pink'];
 function timer(){
     for(var i=0;i<div.length;i++){
-        div[i].style.background ="orange";
+        div[i].style.background="orange";
     };
-    var a =ner();
+    var a=ner();
     for(var m=0;m<3;m++){
         div[a[m]].style.background=colors[m];
     };
@@ -15,18 +15,21 @@ function ner(){
     var sum1=Math.floor(Math.random()*9);
     var sum2=Math.floor(Math.random()*9);
     var sum3=Math.floor(Math.random()*9);
+//				console.log(Math.floor)
+//				console.log(Math.random())
+//				console.log(Math.floor(Math.random()*9))
     while(sum2==sum1){
-        sum2=Math.floor(Math.random()*9)
+        sum2=Math.floor(Math.random()*9);
     }
     while(sum3==sum2||sum3==sum1){
         sum3=Math.floor(Math.random()*9);
     }
-    a.push(sum1,sum2,sum3);
+    a.push(sum1,sum2 ,sum3);
     return a
 }
 var times;
 var btn1=document.getElementById('start');
-
+var btn2=document.getElementById('over');
 btn1.onclick=function(){
     clearInterval(times);
     times=setInterval(timer,1000);
@@ -34,14 +37,14 @@ btn1.onclick=function(){
     btn1.className='change';
 };
 btn2.onclick=function(){
-    btn2.className='';
+    btn2.className=' ';
     btn1.className='';
     clearInterval(times);
-    setTimeout(function () {
+    setTimeout(function(){
         btn2.className='';
     },1000)
     for(var x=0;x<div.length;x++){
-        div[x].style.background='aornge';
+        div[x].style.background="orange";
     };
 };
 
